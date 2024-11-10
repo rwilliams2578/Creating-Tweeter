68d62bb6d235ca60404f56e96e47c7dc279848fb
Revelle Williams <rwilliams2578@mail.kvcc.edu>
2024-11-09 19:48:05 -0600

    Create accounts/urls.py, twits/urls.py. Modify accounts/admin.py, accounts/models.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,14 +2,16 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-Add date_of_birth to the fieldsets
+# Add date_of_birth to the fieldsets
+
+
 class CustomUserAdmin(UserAdmin):
     """
     Custom admin configuration for CustomUser model.
     Adds date_of_birth field to the user form.
     """
-    fieldsets = UserAdmin.fieldsets + (
-        ('Additional Info', {'fields': ('date_of_birth',)}),
-    )
+
+    fieldsets = UserAdmin.fieldsets + (("Additional Info", {"fields": ("date_of_birth",)}),)
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class Customuser(AbstractUser):
+class CustomUser(AbstractUser):
     """
     Custom user model that extends Django's AbstractUser.
     Simply adds a date of birth field.
