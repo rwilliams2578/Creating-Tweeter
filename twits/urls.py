83856f4483c19577f90f7e9564f3ddb12dd533ea
Revelle Williams <rwilliams2578@mail.kvcc.edu>
2024-11-10 08:04:33 -0600

    Modify accounts/urls.py, twits/urls.py

---
+++ twits/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.DashboardView.as_view(), name="dashboard"),
+    path("twit/new/", views.TwitCreateView.as_view(), name="twit_create"),
+    path("twit/<int:pk>/edit/", views.TwitUpdateView.as_view(), name="twit_update"),
+    path("twit/<int:pk>/delete/", views.TwitDeleteView.as_view(), name="twit_delete"),
+    path("user/<int:pk>/", views.UserPublicProfileView.as_view(), name="public_profile"),
+    path("twit/<int:pk>/like/", views.like_twit, name="like_twit"),
+    path("twit/<int:pk>/comment/new/", views.CommentCreateView.as_view(), name="comment_new"),
+]
