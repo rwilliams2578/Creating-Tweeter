fcb286798f0b8dcd85c34ed6f06cbd2bbf9ab1e0
Revelle Williams <rwilliams2578@mail.kvcc.edu>
2024-11-28 14:42:52 -0600

    Update templates/registration/login.html, templates/twits/comment_form.html, twits/models.py, templates/twits/twit_form.html. And Makemigrations

--- django_project/settings.py
+++ django_project/settings.py
@@ -44,6 +44,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "twits.apps.TwitsConfig",
+    # Third Party
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
--- twits/models.py
+++ twits/models.py
@@ -47,6 +47,7 @@ class Comment(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     body = models.TextField(max_length=140)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ["-created_at"]
